93ffbbdb3060623d344b033a2751bf37d98d44d4
sanjiv kumar <piyush20240109644@asose.in>
2026-06-12 21:53:56 +0530

    Delete ui/launcher directory

--- ui/launcher/include/home_screen.h
+++
@@ -1,8 +0,0 @@
-#ifndef PHONEOS_HOME_SCREEN_H
-#define PHONEOS_HOME_SCREEN_H
-
-#include <lvgl.h>
-
-void phoneos_home_screen_create(lv_obj_t *parent, void (*on_app_selected)(const char *app_id));
-
-#endif
